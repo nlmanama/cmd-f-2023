b02cb54245e3f82c4553a9b8ddc25e731ad8afbd
Flayre <mariesamantha.f@gmail.com>
2023-03-11 15:22:39 -0800

    Added texttospeech feature

---
+++ TextToSpeech/src/Vocalize.js
@@ -0,0 +1,38 @@
+const textarea = document.querySelector("textarea");
+const speechBtn = document.querySelector("button");
+
+let synth = speechSynthesis;
+let isSpeaking = true;
+
+function textToSpeech(text) {
+  let utterance = new SpeechSynthesisUtterance(text);
+  synth.speak(utterance);
+}
+
+speechBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (textarea.value !== "") {
+    if (!synth.speaking) {
+      textToSpeech(textarea.value);
+    }
+    if (textarea.value.length > 80) {
+      setInterval(() => {
+        if (!synth.speaking && !isSpeaking) {
+          isSpeaking = true;
+          speechBtn.innerText = "Speaker Button";
+        }
+      }, 500);
+      if (isSpeaking) {
+        synth.resume();
+        isSpeaking = false;
+        speechBtn.innerText = "Pause";
+      } else {
+        synth.pause();
+        isSpeaking = true;
+        speechBtn.innerText = "Resume";
+      }
+    } else {
+      speechBtn.innerText = "Speaker Button";
+    }
+  }
+});
