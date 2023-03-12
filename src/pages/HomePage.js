fc030ee026d949023f2bad23c099592dc4275fa1
nlmanama <nlminhanh12@gmail.com>
2023-03-11 21:06:56 -0800

    make bg better

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,20 +3,19 @@ import "./Style.css";
 
 export default function HomePage () {
     return (
-        <div class="row">   
-            <div class="header">
-            <h1>summaRiff</h1>
-            <p>A text-to-speech & speech-to-text summarizer</p>
+        <>
+            <div class="row">   
+                <div class="header">
+                    <h1>summaRiff</h1>
+                    <p>A text-to-speech & speech-to-text summarizer</p>
+                </div>
             </div>
-
             <div class="column left">
-            <p><center>Text</center></p>
+                <p><center>Text</center></p>
             </div>
-
-            
             <div class="column right">
-            <p><center>Summary</center></p>
+                <p><center>Summary</center></p>
             </div>
-        </div>
+        </>
     );
 }
