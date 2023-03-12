61e2e096b05a062b96c61c893cd1278d12ac9598
Radhika Bajaj <rbajaj@students.cs.ubc.ca>
2023-03-11 18:53:00 -0800

    initialize

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage () {
+    return (
+        
+        <body bgcolor="lightblue">
+        <h1><center>sumaRiff</center></h1>
+        </body>
+    );
+}
