a88bf31845422b18c36b0e313cfa61e5250eec90
Radhika Bajaj <rbajaj@students.cs.ubc.ca>
2023-03-11 21:58:49 -0800

    added text-to-speaker box and button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -10,9 +10,18 @@ export default function HomePage () {
                 <p>A text-to-speech & speech-to-text summarizer</p>
             </div>
             
-            <div class="column left">
+            <div class="column left"> <center>
                 <p><center>Text</center></p>
-            </div>
+                <div class="wrapper">
+                <textarea></textarea>
+                </div>
+
+                <div class="button">
+                    <form action="#">
+                        <button>Summarize</button>
+                    </form>
+                </div>
+            </center></div>
             
             <div class="column right"> <center>
                 <p><center>Summary</center></p>
