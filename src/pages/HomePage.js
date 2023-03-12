0c8e26cc38eaa546b5c6091d1b96e4606c9f8024
Radhika Bajaj <rbajaj@students.cs.ubc.ca>
2023-03-11 21:45:04 -0800

    added text-to-speech box and button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,18 +4,32 @@ import "./Style.css";
 export default function HomePage () {
     return (
         <>
-            <div class="row">   
-                <div class="header">
-                    <h1>summaRiff</h1>
-                    <p>A text-to-speech & speech-to-text summarizer</p>
-                </div>
+        <div class="row">   
+            <div class="header">
+                <h1>summaRiff</h1>
+                <p>A text-to-speech & speech-to-text summarizer</p>
             </div>
+            
             <div class="column left">
                 <p><center>Text</center></p>
             </div>
-            <div class="column right">
+            
+            <div class="column right"> <center>
                 <p><center>Summary</center></p>
-            </div>
+            
+                <script src="Vocalize.js"></script>
+                <div class="wrapper">
+                </div>
+
+                <div class="button">
+                    <form action="#">
+                        <button>Speaker Button</button>
+                    </form>
+                </div>
+            </center></div>
+            
+        </div>
+        
         </>
     );
 }
