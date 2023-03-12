ebece594fa89c80699a673bc5263356626cf97ba
Radhika Bajaj <rbajaj@students.cs.ubc.ca>
2023-03-11 20:48:47 -0800

    added collumns

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,22 @@
 import React from "react";
+import "./Style.css";
 
 export default function HomePage () {
     return (
-        
-        <body bgcolor="lightblue">
-        <h1><center>sumaRiff</center></h1>
-        </body>
+        <div class="row">   
+            <div class="header">
+            <h1>summaRiff</h1>
+            <p>A text-to-speech & speech-to-text summarizer</p>
+            </div>
+
+            <div class="column left">
+            <p><center>Text</center></p>
+            </div>
+
+            
+            <div class="column right">
+            <p><center>Summary</center></p>
+            </div>
+        </div>
     );
 }
